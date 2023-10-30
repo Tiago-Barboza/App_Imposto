84ed497606a7c943722bb70e4aba6980d8885067
Tiago_Barboza <tiago.barbozasantos@outlook.com>
2023-10-29 21:00:58 -0300

    Version 1.9

--- project_imposto/settings.py
+++ project_imposto/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'project_imposto.urls'
@@ -107,12 +108,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
